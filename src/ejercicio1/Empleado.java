20693b54bdd0fae89bcd79a691bd15e342089d22
Tomas Medel <tfmedel@gmail.com>
2024-03-27 13:47:58 -0300

    Crear constructor con parametros
    Crear un constructor vacio, y otro que reciba parametros
    el que esta vacio se le asignara a nombre sin nombre, a
    edad 99 y estado true
    en el cosntructor que recibe parametros tomara sus
    parametros y estado true

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -6,5 +6,18 @@ public class Empleado
 	 private String nombre;
 	 private int edad;
 	 private boolean estado;
+
+	 public Empleado()
+	 {
+		 this.nombre = "sin nombre";
+		 this.edad = 99;
+		 this.estado = true;
+	 }
 	 
+	 public Empleado(String nombre, int edad)
+	 {
+		 this.nombre = nombre;
+		 this.edad = edad;
+		 this.estado = true;
+	 }
 }
